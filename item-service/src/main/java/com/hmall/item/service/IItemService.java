0a39fc583a66134bd67bdd0e3a8ef7ac27b022a0
root <147520338+root-akina@users.noreply.github.com>
2023-11-17 21:48:00 +0800

    商城实战-商品分页

--- item-service/src/main/java/com/hmall/item/service/IItemService.java
+++ item-service/src/main/java/com/hmall/item/service/IItemService.java
@@ -1,7 +1,10 @@
 package com.hmall.item.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.common.dto.PageDTO;
 import com.hmall.item.pojo.Item;
 
 public interface IItemService extends IService<Item> {
+
+    PageDTO selectPage(Integer page, Integer size);
 }
--- item-service/src/main/java/com/hmall/item/web/ItemController.java
+++ item-service/src/main/java/com/hmall/item/web/ItemController.java
@@ -1,14 +1,27 @@
 package com.hmall.item.web;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.dto.PageDTO;
+import com.hmall.item.pojo.Item;
 import com.hmall.item.service.IItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("item")
 public class ItemController {
 
     @Autowired
     private IItemService itemService;
+
+    @GetMapping("/list")
+    public PageDTO pageQuery(@RequestParam("page") Integer page,@RequestParam("size") Integer size){
+        log.info("分页参数：{}+{}",page,size);
+        return itemService.selectPage(page,size);
+    }
 }
