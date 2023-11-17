e2942d8443ed2120cbf536ef4d9fd98672669a99
root <147520338+root-akina@users.noreply.github.com>
2023-11-17 21:48:06 +0800

    商城实战-商品分页查询

--- item-service/src/main/java/com/hmall/item/mapper/ItemMapper.java
+++ item-service/src/main/java/com/hmall/item/mapper/ItemMapper.java
@@ -2,6 +2,11 @@ package com.hmall.item.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hmall.item.pojo.Item;
+import org.apache.ibatis.annotations.Select;
 
 public interface ItemMapper extends BaseMapper<Item> {
+
+    @Select("select count(id) from test02.tb_item")
+    Long selectAll();
+
 }
--- item-service/src/main/java/com/hmall/item/service/impl/ItemService.java
+++ item-service/src/main/java/com/hmall/item/service/impl/ItemService.java
@@ -1,11 +1,34 @@
 package com.hmall.item.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.common.dto.PageDTO;
 import com.hmall.item.mapper.ItemMapper;
 import com.hmall.item.pojo.Item;
 import com.hmall.item.service.IItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService extends ServiceImpl<ItemMapper, Item> implements IItemService {
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+
+    @Override
+    public PageDTO selectPage(Integer pages, Integer size) {
+        Page<Item> page = new Page<>(pages, size);
+//2.构建条件对象
+        LambdaQueryWrapper<Item> queryWrapper = new LambdaQueryWrapper<>();
+
+//3.调用selectPage()方法
+        Page<Item> pageResult = itemMapper.selectPage(page, queryWrapper);
+        List<Item> itemList = pageResult.getRecords();
+        long total = pageResult.getTotal();
+        return new PageDTO<>(total,itemList);
+    }
 }
