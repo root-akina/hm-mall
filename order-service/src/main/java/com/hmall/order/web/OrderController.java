24e162575cccf58ea50b3461363b19e637359239
root <147520338+root-akina@users.noreply.github.com>
2023-11-20 18:19:53 +0800

    商城实战-Item-OrderDetail

--- order-service/src/main/java/com/hmall/order/web/OrderController.java
+++ order-service/src/main/java/com/hmall/order/web/OrderController.java
@@ -16,14 +16,18 @@ public class OrderController {
     private IOrderService orderService;
 
     @GetMapping("{id}")
-    public Order queryOrderById(@PathVariable("id") Long orderId) {
+    public Order queryOrderById(@PathVariable("id") String  orderId) {
+        System.out.println(orderId);
         return orderService.getById(orderId);
     }
 
     @PostMapping
-    public Long orderConfirm(@RequestBody OrderDTO orderDTO) {
+    public String  orderConfirm(@RequestBody OrderDTO orderDTO) {
         log.info("创建订单：数量：{},付款方式：{},收货人地址：{},商品Id：{}", orderDTO.getNum(), orderDTO.getPaymentType(), orderDTO.getAddressId(), orderDTO.getItemId());
         Order order = orderService.confirmOrder(orderDTO);
-        return order.getId();
+        orderService.confirmOrderDetail(order,orderDTO);
+        String orderId = String.valueOf(order.getId());
+        System.out.println(orderId);
+        return orderId;
     }
 }
