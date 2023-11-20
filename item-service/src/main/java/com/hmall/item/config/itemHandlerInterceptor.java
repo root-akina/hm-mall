38b5a81ff8b85376ea92f54933079518e4f215e2
root <147520338+root-akina@users.noreply.github.com>
2023-11-20 09:14:07 +0800

    商城实战-Gateway-Item服务Thread Local

--- item-service/src/main/java/com/hmall/item/config/itemHandlerInterceptor.java
+++ item-service/src/main/java/com/hmall/item/config/itemHandlerInterceptor.java
@@ -1,11 +1,14 @@
 package com.hmall.item.config;
 
+import com.hmall.common.context.BaseContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @Configuration
 public class itemHandlerInterceptor implements HandlerInterceptor {
 
@@ -18,8 +21,9 @@ public class itemHandlerInterceptor implements HandlerInterceptor {
         // 解析 authorization 信息，获取 userId
 
         // 将 userId 存储到 ThreadLocal 中
-        System.out.println("@@@@@@@@@@@@@@@@"+authorizationHeader);
-
+        Long id = Long.valueOf(authorizationHeader);
+        BaseContext.setCurrentId(id);
+        log.info("Item服务—用户id：{}",id);
         // 返回 true 表示继续执行后续的拦截器和处理器
         return true;
     }
